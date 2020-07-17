887cb5a1ab7cdc53bc3f8a0a7db77ff1bd9d84cb
GuangHan Pan(潘广涵) <ext-ghpan@Hillstonenet.com>
2020-07-17 10:27:09 +0800

    add markdown

--- article/views.py
+++ article/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from .models import ArticlePost
+import markdown
 
 def article_list(request):
     articles = ArticlePost.objects.all()
@@ -10,5 +10,10 @@ def article_list(request):
 
 def article_detail(request, id):
     article = ArticlePost.objects.get(id=id)
+    article.body = markdown.markdown(article.body,
+    extensions=[
+    'markdown.extensions.extra',
+    'markdown.extensions.codehilite',
+    ])
     context = { 'article': article }
     return render(request, 'article/detail.html', context)

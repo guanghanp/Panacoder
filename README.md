1c73b1b74bbfad7e2157cba76f837fdc6368fe05
GuangHan Pan(潘广涵) <ext-ghpan@Hillstonenet.com>
2020-07-20 18:03:11 +0800

    add article and delete

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# super-potato
+# PanaCoder
--- article/urls.py
+++ article/urls.py
@@ -6,4 +6,6 @@ app_name = 'article'
 urlpatterns = [
     path('list/', views.article_list, name='article_list'),
     path('detail/<int:id>/', views.article_detail, name='article_detail'),
+    path('create/', views.article_create, name='article_create'),
+    path('delete/<int:id>/', views.article_delete, name='article_delete'),
 ]

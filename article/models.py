0433147e9a1ab7cba312877cf800e44eed5d2954
Guanghan Pan <gpan@middlebury.edu>
2020-07-22 18:09:22 +0800

    add lots of small functionalities

--- article/models.py
+++ article/models.py
@@ -2,13 +2,42 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
+
+# Data Model for Article Post
+class ArticleColumn(models.Model):
+
+    # title for column
+    title = models.CharField(max_length=100, blank=True)
+    # time of creation
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
 
 # Data Model for Article Post
 class ArticlePost(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
     title = models.CharField(max_length=100)
     body = models.TextField()
     total_views = models.PositiveIntegerField(default=0)
+    column = models.ForeignKey(
+        ArticleColumn,
+        null=True,
+        blank=True,
+        on_delete=models.PROTECT,
+        related_name='article'
+    )
+    image = ProcessedImageField(
+        upload_to='article/%Y%m%d',
+        processors=[ResizeToFit(width=400)],
+        format='JPEG',
+        null=True,
+        blank=True,
+        options={'quality': 100},
+    )
     created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
 
@@ -20,3 +49,4 @@ class ArticlePost(models.Model):
 
     def get_absolute_url(self):
         return reverse('article:article_detail', args=[self.id])
+
--- comment/models.py
+++ comment/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from article.models import ArticlePost
 
 
-# Data Model for Article Post
+# Data Model for Comment
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     article = models.ForeignKey(ArticlePost, on_delete=models.CASCADE, related_name='comments')
